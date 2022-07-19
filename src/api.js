49b3cb7374e55307fb751755637cf52e93c463cd
akosijose <josegeraldlumbao0307@gmail.com>
2022-07-19 10:28:02 +0800

    completed weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cdd58554c3msh8849d25341de403p1c409djsn5e9e1f6789b6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "964e53404dfdbf18b4a4ffd86806d35c";
